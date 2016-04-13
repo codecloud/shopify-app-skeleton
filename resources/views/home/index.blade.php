4f246778506deb08253997552481060ef081d84b
David Smith <dasmith@rmn.com>
2016-04-13 17:20:24 +0100

    Fixed template var names

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     <div class="container">
         <div class="col-sm-4 col-sm-4-offset">
-            <a href="{{ install_url }}" class="btn btn-primary">Install Application</a>
+            <a href="{{ installUrl }}" class="btn btn-primary">Install Application</a>
         </div>
     </div>
 @endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>@yield('title') - {{ siteName }}</title>
+    <title>@yield('title') - Imagine Apps</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="" />
